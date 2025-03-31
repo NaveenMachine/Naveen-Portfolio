0b5970e17e1c5793cde5e9611159aa2ba46906cd
KamathN593 <nk2017@kamaths.com>
2025-03-31 16:59:58 -0400

    image swiper comp

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -4,14 +4,14 @@ import React from "react";
 //import { Spotlight } from "./ui/Spotlight";
 //import { TextGenerateEffect } from "./ui/TextGenerateEffect";
 import { Box, Grid, Paper, Typography, IconButton } from '@mui/material';
-import ImageSlider from "./ImageSlider";
+import ImageSwiper from "./ImageSlider";
 
 const About = () => {
   return (
     <Box display='flex' justifyContent='center' alignItems='center' sx={{ backgroundColor: 'secondary.main', height: '50vh', width: '100vw', }}>
       <Grid container spacing={2} my={4}>
         <Grid item xs={12} sm={6} md={4}>
-          <Paper sx={{ p: 2 }}><ImageSlider /></Paper>
+          <Paper sx={{ p: 2 }}><ImageSwiper /></Paper>
         </Grid>
         <Grid item xs={12} sm={6} md={4}>
           <Paper sx={{ p: 2 }}>Based in the Dayton and studying in Columbus, I'm an experienced software engineer with extensive knowledge in full-stack development. Aside from my unhealthy obsession with all things science fiction, in my free time I enjoy being an amateur musician and dedicated gym rat.
--- src/components/About/ImageSlider.jsx
+++ src/components/About/ImageSlider.jsx
@@ -1,14 +1,51 @@
-import React from "react";
+//import React from "react";
 //import { FaLocationArrow } from "react-icons/fa6";
 //import MagicButton from "./MagicButton";
 //import { Spotlight } from "./ui/Spotlight";
 //import { TextGenerateEffect } from "./ui/TextGenerateEffect";
-import { Box, Grid, Paper, Typography, IconButton } from '@mui/material';
+import React from "react";
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
 
-const ImageSlider = () => {
+// Define ImageSwiper component
+const ImageSwiper = () => {
   return (
-    <></>
+    <Swiper
+      spaceBetween={50}
+      slidesPerView={3}
+      onSlideChange={() => console.log("Slide change")}
+      onSwiper={(swiper) => console.log(swiper)}
+    >
+      <SwiperSlide>
+        <img
+          src="https://via.placeholder.com/300"
+          alt="Slide 1"
+          style={{ width: "100%", height: "auto" }}
+        />
+      </SwiperSlide>
+      <SwiperSlide>
+        <img
+          src="https://via.placeholder.com/300"
+          alt="Slide 2"
+          style={{ width: "100%", height: "auto" }}
+        />
+      </SwiperSlide>
+      <SwiperSlide>
+        <img
+          src="https://via.placeholder.com/300"
+          alt="Slide 3"
+          style={{ width: "100%", height: "auto" }}
+        />
+      </SwiperSlide>
+      <SwiperSlide>
+        <img
+          src="https://via.placeholder.com/300"
+          alt="Slide 4"
+          style={{ width: "100%", height: "auto" }}
+        />
+      </SwiperSlide>
+    </Swiper>
   );
 };
 
-export default ImageSlider;
+export default ImageSwiper;
