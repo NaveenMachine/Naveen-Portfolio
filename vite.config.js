faa3b9038d557ffea1ffcd66794c93d389939e0f
KamathN593 <nk2017@kamaths.com>
2025-05-17 15:59:59 -0400

    fixed 'npm run deploy'

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/Naveen-Portfolio/', // Replace <repo-name> with your GitHub repository name
+  base: '/', // Replace <repo-name> with your GitHub repository name
 });
